34ef44a7f580cd662b749eefef5b84a9bee0ebe4
Alex Dunn <dunn.alex@gmail.com>
2016-12-06 21:01:00 -0800

    mtgbot: update to new Slack API

--- nerd/run_nerd.js
+++ nerd/run_nerd.js
@@ -1,274 +1,254 @@
-#!/usr/bin/env node
+const cheerio = require('cheerio')
+const mtg = require('mtg-json')
+const request = require('request')
 
-var request = require('request');
-var cheerio = require('cheerio');
-var Slack = require('slack-client');
+const RtmClient = require('@slack/client').RtmClient
+const TOKEN = process.env.SLACK_TOKEN
 
-var token = require('./secrets.js').slack,
-    autoReconnect = true,
-    autoMark = true;
-
-var slack = new Slack(token, autoReconnect, autoMark);
-
-var blacklist = [
+const blacklist = [
   'Our Market Research Shows That Players Like Really Long Card Names So We Made this Card to Have the Absolute Longest Card Name Ever Elemental'
-];
+]
 
-var random = {
+const random = {
   integer: function(min,max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+    return Math.floor(Math.random() * (max - min + 1)) + min
   },
   index: function(arr) {
-    var index = Math.floor(arr.length*Math.random());
-    return arr[index];
+    var index = Math.floor(arr.length*Math.random())
+    return arr[index]
   }
-};
+}
 
 // functions for rendering the card attributes in the mtgapi object
-var attributes = {
-  rarity: function(card) {
-    return card.rarity;
-  },
+const attributes = {
   colors: function(card) {
-    return (card.colors ? card.colors.join("-") : "colorless");
+    return (card.colors ? card.colors.join("-") : "colorless")
   },
   power: function(card) {
-    return (card.power ? card.power : false);
+    return card.power
   },
   toughness: function(card) {
-    return (card.toughness ? card.toughness : false);
+    return card.toughness
   },
   supertypes: function(card) {
-    if (card.supertypes) {
-      return card.supertypes.join(" ");
-    }
-    else return false;
+    if (card.supertypes)
+      return card.supertypes.join(" ")
+    else return false
   },
   types: function(card) {
-    return card.types.join(" ");
+    return card.types.join(" ")
   },
   subtypes: function(card) {
-    if (card.subtypes) {
-      return card.subtypes.join(" ");
-    }
-    else return false;
+    if (card.subtypes)
+      return card.subtypes.join(" ")
+    else return false
   },
   set: function(card) {
-    return "from " + card.printings[0];
+    return "from " + card.printings[0]
   },
   manaCost: function(card) {
-    return "costing " + card.manaCost;
+    return "costing " + card.manaCost
   },
   text: function(card) {
-    if (card.text && card.text.replace(/\s*/g,"")) {
-      return "that reads: “" + card.text + "”";
-    }
-    else return false;
+    if (card.text && card.text.replace(/\s*/g,""))
+      return "that reads: “" + card.text + "”"
+    else return false
   },
   flavor: function(card) {
-    if (card.flavor && card.flavor.replace(/\s*/g,"")) {
-      return "whose flavor text is “" + card.flavor + "”";
-    }
-    else return false;
+    if (card.flavor && card.flavor.replace(/\s*/g,""))
+      return "whose flavor text is “" + card.flavor + "”"
+    else return false
   }
-};
+}
 
-function ngrams(array,n) {
+function ngrams(array, n) {
 // Adapted from Natural
 // (https://github.com/NaturalNode/natural/blob/master/lib/natural/ngrams/ngrams.js#L45):
 // copyright (c) 2011, Rob Ellis, Chris Umbel
-  var result = [];
-  var count = array.length - n + 1;
-  for (var i = 0; i < count; i++) {
-    // console.log(array.slice(i, i + n));
-    result.push(array.slice(i, i + n));
+  var result = []
+  const count = array.length - n + 1
+
+  for (var i = 0; i < count; i++)
+    result.push(array.slice(i, i + n))
+
+  return result
+}
+
+function info(pick, callback) {
+  // '+l%3Aen' specifies English-language results
+  const query = `http://magiccards.info/query?q=${pick}+l%3Aen&v=card&s=cname`
+
+  const reqOpts = {
+    url: query,
+    headers: {
+      'User-Agent': 'SLACK DOT COM OH HELL'
+    }
   }
-  return result;
-}
 
-function info(pick,callback) {
-  var query = "http://magiccards.info/query?q=" +
-        // '+l%3Aen' specifies English-language results
-        pick + "+l%3Aen&v=card&s=cname";
   // run an search on magiccards.info and pluck a random result
-  request({url: query}, function(err,response,body) {
-    if (err) return callback(err);
-    if (body.match("Your query did not match any cards.")) {
-      return callback(new Error('No results for ' + pick));
-    }
-    var $ = cheerio.load(body);
+  request(reqOpts, (err, response, body) => {
+    if (err)
+      return callback(err)
 
+    if (body.match("Your query did not match any cards."))
+      return callback(new Error('No results for ' + pick))
 
-    var results = $('table[align=center]');
+    const $ = cheerio.load(body)
     // the first centered table is not part of the search results
+    const results = $('table[align=center]')
 
-    var mtgName, href, isBlacklisted;
-    while (!mtgName || isBlacklisted) {
-      var randomPick = random.integer(1, results.length - 1);
-      var selection = results.eq(randomPick).children().first().children().eq(1).children().first(); // lol
+    var mtgName, href, isBlacklisted
 
-      // remove whitespace and linebreaks
-      mtgName = selection.text().replace(/\\\\n/g,"");
-      mtgName = mtgName.replace(/^\s*/g,"");
-      mtgName = mtgName.replace(/\s*$/g,"");
+    const randomPick = random.integer(1, results.length - 1)
+    const selection = results.eq(randomPick).children().first().children().eq(1).children().first() // lol
 
-      href = selection.children().first().attr('href');
+    console.log(`selection: ${selection}`)
 
-      isBlacklisted = blacklist.some(function(value,index,array){
-        return value === mtgName;
-      });
-    }
+    // remove whitespace and linebreaks
+    mtgName = selection.text().replace(/\\\\n/g,"")
+    mtgName = mtgName.trim()
 
-    if (mtgName && href) {
-      return callback(null,{name: mtgName, href: href});
-    }
-    else {
-      return callback(new Error('Bad response for query: ' + pick));
-    }
-  });
+    href = selection.children().first().attr('href')
+
+    isBlacklisted = blacklist.some((value, index, array) => {
+      return value === mtgName
+    })
+
+    if (mtgName && href && !isBlacklisted)
+      return callback(null,{name: mtgName, href: href})
+    else
+      return callback(new Error('Bad response for query: ' + pick))
+  })
 }
+
+const rtm = new RtmClient(TOKEN)
+const CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS
+
+// The client will emit an RTM.AUTHENTICATED event on successful connection, with the `rtm.start` payload if you want to cache it
+rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, (rtmStartData) => {
+  console.log('[hacker voice] im in')
+})
+
+rtm.start()
 
 //////// needs to persist across message events
-var currentCard = {};
+const currentCard = {}
 
-slack.on('message', function(message) {
-  if (message.type === 'message' && message.text) {
-    var channel = slack.getChannelGroupOrDMByID(message.channel);
+rtm.on('message', (message) => {
+  if (!(message.type === 'message' && message.text))
+    return;
 
-    // keep a separate record for each channel the bot is in
-    if (!currentCard[message.channel]) {
-      currentCard[message.channel] = {};
-    }
+  console.log(message);
 
-    if (currentCard[message.channel].time) {
-      var diff = Date.now() - currentCard[message.channel].time;
-      // if five minutes have elapsed and the card name hasn't been
-      // guessed, reset
-      if (diff > 300000) {
-        currentCard[message.channel] = {};
-      }
-    }
+  // keep a separate record for each channel the bot is in
+  if (!currentCard[message.channel])
+    currentCard[message.channel] = {}
 
-    // if we're waiting for someone to guess the right answer
-    if (currentCard[message.channel].name) {
-      currentCard[message.channel].name = currentCard[message.channel].name.toLowerCase();
-      var answer = message.text.toLowerCase().indexOf(currentCard[message.channel].name);
-      if (answer > -1) {
-        channel.send('HELL YEAH OH YEAH ' + currentCard[message.channel].url);
-        currentCard[message.channel] = {};
-      }
-    }
+  if (currentCard[message.channel].time) {
+    const diff = Date.now() - currentCard[message.channel].time
+    // if five minutes have elapsed and the card name hasn't been
+    // guessed, reset
+    if (diff > 300000)
+      currentCard[message.channel] = {}
+  }
 
-    // if we're not waiting for someone to guess the right answer
-    else {
-      var tokens = message.text.split(" ");
-
-      var pick = '';
-      if (tokens.length > 1) {
-        var bigrams = ngrams(tokens,2);
-        pick = random.index(bigrams).join(" ");
-      }
-      else {
-        pick = message.text;
-      }
-
-      info(pick, function(err,card) {
-        if (err) return err;
-
-        var path = card.href.replace(/html/,"jpg");
-        path = path.replace(/^\/([^\/]*)\/([^\/]*)/,"$2/$1");
-        var img = 'http://magiccards.info/scans/' + path;
-
-        var apiQuery = 'http://api.mtgapi.com/v2/cards?name=' +
-              encodeURIComponent(card.name);
-
-        request({url: apiQuery}, function(err,response,body) {
-          if (err) return err;
-          var json = JSON.parse(body);
-
-          if (!json.cards) {
-            console.log(json);
-            return new Error('Bad API response');
-          }
-          var card = JSON.parse(body).cards[0];
-
-          // don't even bother if the card name is in the card text
-          // (if there is card text)
-          if (!card.text || card.text.indexOf(card.name) < 0) {
-            currentCard[message.channel].name = card.name;
-            currentCard[message.channel].url = img;
-            currentCard[message.channel].time = Date.now();
-
-            var reply = "";
-
-            var introductions = [
-              "hey did i just hear someone mention the",
-              "oh cool are we talking about the",
-              "glad i'm not the only one thinking about the",
-              "haha nice that reminds me of the"
-            ];
-            reply += random.index(introductions);
-            reply += " ";
-
-            var misc = [
-              attributes.rarity(card),
-              attributes.colors(card)
-            ];
-            reply += random.index(misc);
-            reply += " ";
-
-            if (card.power && card.toughness &&
-                random.integer(0,1) === 1) {
-              var pt = [
-                attributes.power(card),
-                attributes.toughness(card)
-              ].join("/");
-              reply += pt;
-              reply += " ";
-            }
-
-            var supType = attributes.supertypes(card);
-            if (supType && random.integer(0,1) === 1) {
-              reply += supType + " ";
-            }
-
-            var subType = attributes.subtypes(card);
-            if (subType && random.integer(0,1) === 1) {
-              reply += subType + " ";
-            }
-
-            reply += attributes.types(card);
-            reply += " ";
-
-            var misc2 = [
-              attributes.set(card),
-              attributes.manaCost(card)
-            ];
-            reply += random.index(misc2);
-            reply += " ";
-
-            var text = attributes.text(card);
-            if (text) {
-              reply += text;
-              reply += " ";
-            }
-
-            // var flavor = attributes.flavor(card);
-            // if (flavor && random.integer(0,2) === 1) {
-            //   reply += flavor;
-            // }
-
-            console.log(reply + ' (' + card.name + ')');
-            return channel.send(reply);
-          }
-        });
-      });
+  // if we're waiting for someone to guess the right answer
+  if (currentCard[message.channel].name) {
+    currentCard[message.channel].name = currentCard[message.channel].name.toLowerCase()
+    const answer = message.text.toLowerCase().indexOf(currentCard[message.channel].name)
+    if (answer > -1) {
+      rtm.sendMessage(`HELL YEAH OH YEAH ${currentCard[message.channel].url}`, message.channel)
+      currentCard[message.channel] = {}
     }
   }
-});
 
-slack.on('error', function(error) {
-  console.error('Error: %s', error);
-});
+  // if we're not waiting for someone to guess the right answer
+  else {
+    const tokens = message.text.split(" ")
 
-slack.login();
+    let pick
+    if (tokens.length > 1) {
+      const bigrams = ngrams(tokens,2)
+      pick = random.index(bigrams).join(" ")
+    }
+    else {
+      pick = message.text
+    }
+
+    console.log(`searching for ${pick}`)
+
+    // card is { name, href }
+    info(pick.replace(/\ /, '+'), (err, response) => {
+      if (err)
+        return err
+
+      const path = response.href.replace(/html/,"jpg")
+            .replace(/^\/([^\/]*)\/([^\/]*)/,"$2/$1")
+      const img = 'http://magiccards.info/scans/' + path
+
+      mtg('cards', __dirname, { extras: true }).then(json => {
+        const card = json[response.name]
+
+        const util = require('util')
+        console.log(util.inspect(card))
+
+        // don't even bother if the card name is in the card text
+        if (card.text && card.text.indexOf(card.name) >= 0) {
+          console.log('The card name is in the text, forget it.')
+          return
+        }
+
+        currentCard[message.channel].name = card.name
+        currentCard[message.channel].url = img
+        currentCard[message.channel].time = Date.now()
+
+        const introductions = [
+          "hey did i just hear someone mention the",
+          "oh cool are we talking about the",
+          "glad i'm not the only one thinking about the",
+          "haha nice that reminds me of the"
+        ]
+
+        let reply = `${random.index(introductions)} ${attributes.colors(card)} `
+
+        if (card.power && card.toughness && random.integer(0,1) === 1) {
+          var pt = [
+            attributes.power(card),
+            attributes.toughness(card)
+          ].join("/")
+          reply += `${pt} `
+        }
+
+        var supType = attributes.supertypes(card)
+        if (supType && random.integer(0,1) === 1) {
+          reply += `${supType} `
+        }
+
+        var subType = attributes.subtypes(card)
+        if (subType && random.integer(0,1) === 1) {
+          reply += `${subType} `
+        }
+
+        reply += `${attributes.types(card)} `
+
+        var misc2 = [
+          attributes.set(card),
+          attributes.manaCost(card)
+        ]
+        reply += `${random.index(misc2)} `
+
+        var text = attributes.text(card)
+        if (text)
+          reply += `${text} `
+
+        // var flavor = attributes.flavor(card)
+        // if (flavor && random.integer(0,2) === 1) {
+        //   reply += flavor
+        // }
+
+        console.log(`${reply} (${card.name})`)
+        return rtm.sendMessage(reply, message.channel)
+      })
+    })
+  }
+})
